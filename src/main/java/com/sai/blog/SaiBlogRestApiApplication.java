b956e37c2c4b136c751985c913e8c1e464e92a8b
Sai Sriram Vundavalli <saisriramv31@gmail.com>
2022-04-01 10:41:24 +0530

    Profiles and Deployment into AWS

--- src/main/java/com/sai/blog/SaiBlogRestApiApplication.java
+++ src/main/java/com/sai/blog/SaiBlogRestApiApplication.java
@@ -1,33 +1,44 @@
 package com.sai.blog;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
-import com.sai.blog.entity.User;
-import com.sai.blog.repository.UserRepository;
+import com.sai.blog.entity.Role;
+import com.sai.blog.repository.RoleRepository;
 
 @SpringBootApplication
-public class SaiBlogRestApiApplication {
+public class SaiBlogRestApiApplication implements CommandLineRunner{
 
 	@Bean
-	public ModelMapper modelMapper()
-	{
+	public ModelMapper modelMapper() {
 		return new ModelMapper();
 	}
-	
+
 	public static void main(String[] args) {
 		ApplicationContext run = SpringApplication.run(SaiBlogRestApiApplication.class, args);
 		
-		UserRepository userRepo = run.getBean(UserRepository.class);
-		PasswordEncoder encoder = run.getBean(PasswordEncoder.class);
-		User user = userRepo.findByUsername("sai").get();
+
+	}
+	
+	@Autowired
+	private RoleRepository roleRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
 		
-		System.out.println(encoder.encode("sai"));
-		System.out.println(encoder.matches("sai", user.getPassword()));
+		Role adminRole = new Role();
+		adminRole.setName("ROLE_ADMIN");
+		roleRepository.save(adminRole);
+		
+		
+		Role userRole = new Role();
+		userRole.setName("ROLE_USER");
+		roleRepository.save(userRole);
 	}
 
 }
